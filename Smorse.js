0656778a2b971ce3b6bc61c1a94cf4d7ad5ea9c0
Liwei <liweihu12@gmail.com>
2019-09-30 22:37:38 +0200

    Update Smorse.js

--- Smorse.js
+++ Smorse.js
@@ -1,3 +1,5 @@
+//https://www.reddit.com/r/dailyprogrammer/comments/cmd1hb/20190805_challenge_380_easy_smooshed_morse_code_1/
+
 let morse =
   ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..";
 let morseArray = morse.split(" ");
